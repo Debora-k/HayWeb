0e0735db6b6f95b5472bf0cc1f51f28b4f5a0228
Debora-k <debora0104k@gmail.com>
2025-05-07 22:29:45 -0600

    added root.tsx

---
+++ app/root.tsx
@@ -0,0 +1,16 @@
+import { Meta, Outlet } from "react-router";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+      </head>
+      <body>
+        <Outlet />
+      </body>
+    </html>
+  );
+}
