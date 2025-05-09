4810a0f029ce52665d77906ec20672bed0b717f7
Debora-k <debora0104k@gmail.com>
2025-05-08 23:22:41 -0600

    added routes

---
+++ app/routes.ts
@@ -0,0 +1,27 @@
+import { type RouteConfig, index, prefix, layout, route } from "@react-router/dev/routes";
+
+export default [
+    index("home/home-pageg.tsx"),
+    ...prefix("about", [
+        index("about/about-page.tsx"),
+    ]),
+    ...prefix("/sign-up", [
+        index("sign-up/sign-up.tsx"),
+    ]),
+    ...prefix("/login", [
+        index("/login/login-page.tsx"),
+    ]),
+    ...prefix("/delivery", [
+        index("/delivery/delivery-page.tsx"),
+    ]),
+    ...prefix("/payment", [
+        index("payment/payment-page.tsx"),
+    ]),
+    ...prefix("/product", [
+        index("/product/product-page.tsx"),
+        route("/order", "order/order-page.tsx"),
+    ]),
+    ...prefix("/contact", [
+        index("/contact/contact-page.tsx"),
+    ]),
+]
