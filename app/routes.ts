0f00713229656989793151e432b406ea70126cc7
Debora-k <debora0104k@gmail.com>
2025-05-13 21:39:21 -0600

    fixed bugs in routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,27 +1,27 @@
 import { type RouteConfig, index, prefix, layout, route } from "@react-router/dev/routes";
 
 export default [
-    index("home/home-pageg.tsx"),
+    index("home/home-page.tsx"),
     ...prefix("about", [
         index("about/about-page.tsx"),
     ]),
     ...prefix("/sign-up", [
-        index("sign-up/sign-up.tsx"),
+        index("sign-up/sign-up-page.tsx"),
     ]),
     ...prefix("/login", [
-        index("/login/login-page.tsx"),
+        index("login/login-page.tsx"),
     ]),
     ...prefix("/delivery", [
-        index("/delivery/delivery-page.tsx"),
+        index("delivery/delivery-page.tsx"),
     ]),
     ...prefix("/payment", [
         index("payment/payment-page.tsx"),
     ]),
     ...prefix("/product", [
-        index("/product/product-page.tsx"),
+        index("product/product-page.tsx"),
         route("/order", "order/order-page.tsx"),
     ]),
     ...prefix("/contact", [
-        index("/contact/contact-page.tsx"),
+        index("contact/contact-page.tsx"),
     ]),
 ]
