0c1effd23c1dcb8622a203c99cca2b7bdcbc7903
Debora-k <debora0104k@gmail.com>
2025-05-19 23:13:54 -0600

    working on navigation

---
+++ app/common/components/navigation.tsx
@@ -0,0 +1,29 @@
+const pages = [
+  {
+    name: "About",
+    to: "/about",
+    items: [],
+  },
+  {
+    name: "Contact",
+    to: "/contact",
+    items: [],
+  },
+  {
+    name: "Delivery",
+    to: "/delivery",
+    items: [],
+  },
+  {
+    name: "Product",
+    to: "/product",
+    items: [],
+  },
+  {
+    name: "Order",
+    to: "/order",
+    items: [],
+  },
+];
+
+export default function Navigation() {}
--- app/contact/contact-page.tsx
+++ app/contact/contact-page.tsx
@@ -2,6 +2,10 @@ export default function ContactPage() {
   return (
     <div>
       <h2>Contact Us</h2>
+      <div>
+        <h4>Email address: 12345@gmail.com </h4>
+        <p>We usually response within 3 days.</p>
+      </div>
     </div>
   );
 }
