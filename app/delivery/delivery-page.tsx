61de7ad83311a9a33f6db830b03e367d4db0adf7
Debora-k <debora0104k@gmail.com>
2025-05-22 21:47:14 -0600

    working on delivery info page

---
+++ app/delivery/delivery-page.tsx
@@ -0,0 +1,12 @@
+export default function DeliveryPage() {
+  return (
+    <div>
+      <h2>Delivery Info</h2>
+      <div>
+        <h4>We are offering delivery service within Calgary!</h4>
+        <p>Minimum order $25 for free delivery</p>
+        <p>If your area is in central of Calgary, then no minimum required</p>
+      </div>
+    </div>
+  );
+}
