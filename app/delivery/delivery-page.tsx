f9c2492c795493826a34af50c937f1ecd926db38
Debora-k <debora0104k@gmail.com>
2025-05-27 23:50:12 -0600

    working on delivery page

--- app/delivery/delivery-page.tsx
+++ app/delivery/delivery-page.tsx
@@ -1,3 +1,7 @@
+import { Form } from "react-router";
+import { Button } from "~/common/components/ui/button";
+import InputPair from "~/common/components/input-pair";
+
 export default function DeliveryPage() {
   return (
     <div>
@@ -6,6 +10,21 @@ export default function DeliveryPage() {
         <h4>We are offering delivery service within Calgary!</h4>
         <p>Minimum order $25 for free delivery</p>
         <p>If your area is in central of Calgary, then no minimum required</p>
+      </div>
+      <div>
+        <Form className="w-full max-w-sm space-y-5" method="post">
+          <InputPair
+            id="address"
+            label="Address"
+            description="Enter your address"
+            name="address"
+            required
+            type="address"
+          />
+          <Button type="submit" className="w-full">
+            Confirm Delivery
+          </Button>
+        </Form>
       </div>
     </div>
   );
