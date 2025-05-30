4c44ec6e254acefa6133df48d616b7888b90a194
Debora-k <debora0104k@gmail.com>
2025-05-29 22:58:43 -0600

    working on sign-up page

--- app/sign-up/sign-up-page.tsx
+++ app/sign-up/sign-up-page.tsx
@@ -1,6 +1,8 @@
 import { Form, Link } from "react-router";
 import { Button } from "~/common/components/ui/button";
 import { z } from "zod";
+import InputPair from "~/common/components/input-pair";
+import AuthButtons from "~/common/components/auth-button";
 
 const formSchema = z.object({
   name: z.string().min(3),
@@ -17,7 +19,48 @@ export default function SignUpPage() {
       </Button>
       <div className="flex items-center flex-col justify-center w-full gap-10">
         <h1 className="text-4xl font-semibold">Create an account</h1>
-        <Form className="w-full max-w-sm space-y-5" method="post"></Form>
+        <Form className="w-full max-w-sm space-y-5" method="post">
+          <InputPair
+            id="name"
+            label="Name"
+            description="Enter your name"
+            name="name"
+            required
+            type="text"
+            placeholder="Enter your name"
+          />
+          <InputPair
+            id="username"
+            label="Username"
+            description="Enter your username"
+            name="username"
+            required
+            type="text"
+            placeholder="e.g. john_doe"
+          />
+          <InputPair
+            id="email"
+            label="Email"
+            description="Enter your email"
+            name="email"
+            required
+            type="email"
+            placeholder="e.g. john@gmail.com"
+          />
+          <InputPair
+            id="password"
+            label="Password"
+            description="Enter your password"
+            name="password"
+            required
+            type="password"
+            placeholder="Enter your password"
+          />
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+          <AuthButtons />
+        </Form>
       </div>
     </div>
   );
