d93747e7cbb340f651bc0b50949d7a242b5346d8
Debora-k <debora0104k@gmail.com>
2025-06-02 23:12:15 -0600

    working on a bug

--- app/logout/logout-page.tsx
+++ app/logout/logout-page.tsx
@@ -1,7 +1,8 @@
 import { redirect } from "react-router";
+import type { Route } from "./+types/logout-page";
+import { makeSSRClient } from "~/supa-client";
 
-export const loader = async () => {
-  return redirect("/");
+export const loader = async ({ request }: Route.ActionArgs) => {
+  const { headers } = makeSSRClient(request);
+  return redirect("/", { headers });
 };
-
-export default function LogoutPage() {}
