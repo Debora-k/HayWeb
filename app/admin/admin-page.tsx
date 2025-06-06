a093d1f6f0eff11ca36535fa1d2dadc98873272e
Debora-k <debora0104k@gmail.com>
2025-06-05 23:49:03 -0600

    added div

--- app/admin/admin-page.tsx
+++ app/admin/admin-page.tsx
@@ -1,1 +1,10 @@
 // this is a page for admins who do manage stocks and update status of order/delivery
+export default function AdminPage() {
+  return (
+    <div>
+      Total Hay Stocks:
+      <div>Order Status </div>
+      <div>User Lists</div>
+    </div>
+  );
+}
