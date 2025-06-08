458ac37a2f923e6234f2eca86e3f92f7c263fe62
Debora-k <debora0104k@gmail.com>
2025-06-07 23:35:14 -0600

    added admin navigation

---
+++ app/common/components/admin-navigation.tsx
@@ -0,0 +1,30 @@
+const navigation = [
+  {
+    name: "Stocks",
+    to: "/stocks",
+    items: [],
+  },
+  {
+    name: "Users",
+    to: "/users",
+    items: [],
+  },
+  {
+    name: "Delivery Status",
+    to: "/delivery",
+    items: [],
+  },
+  {
+    // giving notifications via email
+    name: "Notification",
+    to: "/notification",
+    items: [],
+  },
+  {
+    name: "Orders",
+    to: "/orders",
+    items: [],
+  },
+];
+
+export default function AdminNavigation() {}
