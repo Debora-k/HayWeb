1d71833d2753a96ec6611dcb79dd3be903d75d4d
Debora-k <debora0104k@gmail.com>
2025-06-10 23:56:58 -0600

    added zod for login page

--- app/login/login-page.tsx
+++ app/login/login-page.tsx
@@ -2,6 +2,22 @@ import { Button } from "~/common/components/ui/button";
 import { Form, Link } from "react-router";
 import InputPair from "~/common/components/input-pair";
 import AuthButtons from "~/common/components/auth-button";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email is invalid",
+    })
+    .email("Email is invalid"),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password is invalid",
+    })
+    .min(8, "Password must be at least 8 characters long"),
+});
 
 export default function LoginPage() {
   return (
